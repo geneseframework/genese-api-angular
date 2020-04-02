bd2c90dcc231ed849ef98989b7a11a4fdf8bf2f3
Gilles Fabre <gillesfabrejob@gmail.com>
2020-04-02 09:24:08 +0200

    Adding warning and license

---
+++ construction/models/constants/warning.ts
@@ -0,0 +1,8 @@
+export const WARNING_GENERATED_CODE = `
+	/** 
+	* WARNING: automatically generated code. Please don't add or modify this file.
+	*
+	* @license
+	*
+	* Use of this source code is governed by an MIT-style license
+`;
