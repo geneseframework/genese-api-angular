0e58b79592a15720ba9cafa76d9577e2b75f38a9
Gilles Fabre <geneseframework@gmail.com>
2020-04-08 09:15:35 +0200

    Added comments on construction.ts

--- construction/construction.ts
+++ construction/construction.ts
@@ -8,51 +8,68 @@ const appRootPath = require('app-root-path');
 export class Construction {
 
 
-	private appRoot = appRootPath.toString();
-	public config = new Config();
-    private geneseRequestServiceFactory: GeneseRequestServiceFactory;
-	private openApiFactory = new OpenApiFactory();
+    private appRoot = appRootPath.toString();                           // Path to the root of the app
+    public config = new Config();                                       // Config from .editorconfig file
+    private geneseRequestServiceFactory: GeneseRequestServiceFactory;   // Factory for genese-request.service.ts generation
+    private openApiFactory = new OpenApiFactory();                      // Factory for the datatypes files
 
 
-	constructor() {
-	}
+    constructor() {
+    }
 
 
 
-	startConstruction(): void {
-	    this.getConfig().then(() => {
-	        this.geneseRequestServiceFactory = GeneseRequestServiceFactory.getInstance();
+    /**
+     * Starts the code generation
+     */
+    startConstruction(): void {
+        this.getConfig().then(() => {
+            this.geneseRequestServiceFactory = GeneseRequestServiceFactory.getInstance();
             this.createFolders();
             this.createGeneseRequestService();
             this.createEndpointsServicesAndDataTypes();
         });
-	}
+    }
 
 
 
+    /**
+     * Get editor config from .editorconfig file
+     */
     getConfig(): Promise<any> {
         return this.config.setConfig();
     }
 
 
 
-	createFolders(): void {
-		fse.removeSync(this.appRoot + '/genese');
-		fse.mkdirsSync(this.appRoot + '/genese/genese-api/datatypes');
-		fse.mkdirsSync(this.appRoot + '/genese/genese-api/services');
-	}
+    /**
+     * Creates the genese-api folders
+     */
+    createFolders(): void {
+        fse.removeSync(this.appRoot + '/genese');
+        fse.mkdirsSync(this.appRoot + '/genese/genese-api/datatypes');
+        fse.mkdirsSync(this.appRoot + '/genese/genese-api/services');
+    }
 
 
 
-	createGeneseRequestService(): void {
-		this.geneseRequestServiceFactory.init();
-	}
+    /**
+     * Creates the genese-request.service.ts file
+     */
+    createGeneseRequestService(): void {
+        this.geneseRequestServiceFactory.init();
+    }
 
 
 
-	createEndpointsServicesAndDataTypes(): void {
-		this.openApiFactory.init();
-	}
+    /**
+     * Parses the genese-api.json file in order to :
+     *     - parse the datatypes of the json file in order to create the datatype.ts files
+     *     - parse the endpoint paths in order to add methods to genese-request.service.ts file
+     */
+    createEndpointsServicesAndDataTypes(): void {
+        this.openApiFactory.init();
+    }
 
 }
 
